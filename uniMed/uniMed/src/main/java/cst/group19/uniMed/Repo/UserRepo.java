1c247178aad098f44864c8585c3ee2abe0890fcb
MahthyHasan <mahthyhasan01@gmail.com>
2024-05-11 19:00:07 +0530

    Delete uniMed/uniMed directory

--- uniMed/uniMed/src/main/java/cst/group19/uniMed/Repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package cst.group19.uniMed.Repo;
-
-import cst.group19.uniMed.Entitiy.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-
-public interface UserRepo extends MongoRepository<User, String> {
-}
