95d361cc2d1e7559df9455c76d9efe5d883b81c1
HasanMJMM <maheng907@gmail.com>
2024-05-16 10:47:16 +0530

    update

---
+++ hms/src/Components/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../../Layout/layout";
+
+function Home() {
+  return (
+    <Layout>
+      <div>
+        <h1>Hi</h1>
+      </div>
+    </Layout>
+  );
+}
+
+export default Home;
