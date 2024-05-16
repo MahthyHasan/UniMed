17cee15bfa0d6388c1b2c0dde4ceb17c6e3bf0f0
Mahthy Hasan <96861382+HasanMJMM@users.noreply.github.com>
2024-05-16 12:25:53 +0530

    update

--- unimed-frontend/src/Components/Pages/Home.js
+++ unimed-frontend/src/Components/Pages/Home.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Layout from '../../layout/Layout'
+import React from "react";
+import Layout from "../../layout/Layout";
 
 function Home(props) {
-    return (
-        <Layout>
-            This is home page
-        </Layout>
-    );
+  return (
+    <Layout>
+      <h1>Hi I am Hassan</h1>
+    </Layout>
+  );
 }
 
 export default Home;
