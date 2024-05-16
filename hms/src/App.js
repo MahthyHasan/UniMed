7144e93f8e1815f5311aeddf353fae58efa01f70
Mahthy Hasan <mahthyhasan01@gmail.com>
2024-05-16 10:47:16 +0530

    update

--- hms/src/App.js
+++ hms/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import CreateAccount from "./Components/Pages/CreateAccount";
 import Login from "./Components/Pages/LogIn";
+import Home from "./Components/Pages/Home";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/caccount" element={<CreateAccount />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
