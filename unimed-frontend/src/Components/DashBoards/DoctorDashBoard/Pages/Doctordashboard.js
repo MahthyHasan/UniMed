2d238097a110059445c0442870e46812c3d58485
Mahthy Hasan <96861382+HasanMJMM@users.noreply.github.com>
2024-06-10 04:51:04 +0530

    QrCode InterGrated FirstStep Completed

--- unimed-frontend/src/Components/DashBoards/DoctorDashBoard/Pages/Doctordashboard.js
+++ unimed-frontend/src/Components/DashBoards/DoctorDashBoard/Pages/Doctordashboard.js
@@ -1,31 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Layout from "../../layout/DoctorLayout/DoctorLayouts";
 import Card from "../ComponentsDoctorDashboard/Card";
-import { Scancard } from "../ComponentsDoctorDashboard/Scancard";
+import { ScancardQr } from "../ComponentsDoctorDashboard/ScancardQr";
 import { Channellingactions } from "../ComponentsDoctorDashboard/Channellingactions";
 import { Bookedslots } from "../ComponentsDoctorDashboard/Bookedslots";
+import { QrReader } from "react-qr-reader";
+import QRCodeScanner from "../ComponentsDoctorDashboard/QRCodeScanner";
+
 
 export default function Doctordashboard() {
-	const [username, setUsername] = useState("");
+	const [showAnotherDiv, setShowAnotherDiv] = useState(false);
 
-	useEffect(() => {
-		// Retrieve the username from localStorage
-		const storedUsername = localStorage.getItem("username");
-		if (storedUsername) {
-			setUsername(storedUsername);
-		}
-	}, []);
+	const handleScancardClick = () => {
+		setShowAnotherDiv(true);
+	};
 
 	return (
-		<div>
-			<Layout>
-				<h1>{username}</h1>{" "}
-				{/* Replace 'Doctor Name' with the retrieved username */}
-				<Card />
-				<Scancard />
-				<Channellingactions />
-				<Bookedslots />
-			</Layout>
-		</div>
+		<Layout>
+			<div className="container">
+				<div className="row justify-content-center">
+					<Card />
+				</div>
+				{!showAnotherDiv ? (
+					<div className="row justify-content-center">
+						<div className="col-12 col-md-6">
+							<div className="p-3" onClick={handleScancardClick}>
+								<ScancardQr />
+							</div>
+						</div>
+						<div className="col-12 col-md-6">
+							<div className="p-3">
+								<Channellingactions />
+							</div>
+						</div>
+					</div>
+				) : (
+					<div className="row justify-content-center">
+						<div className="col-12 col-md-6">
+							<div className="p-3">
+								<ScancardQr />
+							</div>
+						</div>
+						<div className="col-12 col-md-6">
+							<div >
+								<QRCodeScanner />
+							</div>
+						</div>
+					</div>
+				)}
+				<div className="row justify-content-center">
+					<Bookedslots />
+				</div>
+			</div>
+		</Layout>
 	);
 }
