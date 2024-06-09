a82739d8c8d31dc0ff009cb8ab0189d75bfcf826
Mahthy Hasan <96861382+HasanMJMM@users.noreply.github.com>
2024-06-09 21:56:21 +0530

    updates

--- unimed-frontend/src/Components/DashBoards/DoctorDashBoard/ComponentsDoctorDashboard/Card.js
+++ unimed-frontend/src/Components/DashBoards/DoctorDashBoard/ComponentsDoctorDashboard/Card.js
@@ -2,47 +2,43 @@ import React from "react";
 import patient from "../../../../assets/icons/patient.svg";
 import totalpatient from "../../../../assets/icons/totalpatient.svg";
 import hours from "../../../../assets/icons/hours.svg";
-
-
+import CardsOnTop from "./CardsOnTop";
 
 export const Card = () => {
-  return (
-    <div className="card--container">
-      <div className="card card-remaining">
-        <div className="card--title">
-          <h6>Remaining Patients in Counter</h6>
-        </div>
-        <div className="card--icon">
-          <img src={patient} alt="patient" className="input-label-icon" />
-        </div>
-        <div className="card--data">
-          <h3>10</h3>
-        </div>
-      </div>
-      <div className="card card-total">
-        <div className="card--title">
-          <h6>Total Channelled Patients</h6>
-        </div>
-        <div className="card--icon">
-          <img src={totalpatient} alt="patient" className="input-label-icon" />
-        </div>
-        <div className="card--data">
-          <h3>20</h3>
-        </div>
-      </div>
-      <div className="card card-hours">
-        <div className="card--title">
-          <h6>Total Channelled Hours for the Day</h6>
-        </div>
-        <div className="card--icon">
-          <img src={hours} alt="hours" className="input-label-icon" />
-        </div>
-        <div className="card--data">
-          <h3>03:40:23</h3>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<div class="col-12 col-md-4">
+				<div>
+					<CardsOnTop
+						cardStyle="remainingPatientCards"
+						cardTitle="Remaining Patients"
+						iconPath={patient}
+						cardBody="25"
+					/>
+				</div>
+			</div>
+			<div class="col-12 col-md-4">
+				<div>
+					<CardsOnTop
+						cardStyle="totalPatientCards"
+						cardTitle="total Patients"
+						iconPath={totalpatient}
+						cardBody="25"
+					/>
+				</div>
+			</div>
+			<div class="col-12 col-md-4">
+				<div>
+					<CardsOnTop
+						cardStyle="totalHoursCards"
+						cardTitle="Worked Hours"
+						iconPath={hours}
+						cardBody="2Hours"
+					/>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Card;
--- unimed-frontend/src/Components/DashBoards/DoctorDashBoard/Pages/Doctordashboard.js
+++ unimed-frontend/src/Components/DashBoards/DoctorDashBoard/Pages/Doctordashboard.js
@@ -17,15 +17,27 @@ export default function Doctordashboard() {
 	}, []);
 
 	return (
-		<div>
-			<Layout>
-				<h1>{username}</h1>{" "}
-				{/* Replace 'Doctor Name' with the retrieved username */}
+		<Layout>
+			<div className="container">
+				<div className="row">
 				<Card />
-				<Scancard />
-				<Channellingactions />
-				<Bookedslots />
-			</Layout>
-		</div>
+				</div>				
+				<div class="row">
+					<div class="col-12 col-md-6">
+						<div>
+							<Scancard />
+						</div>
+					</div>
+					<div class="col-12 col-md-6">
+						<div>
+							<Channellingactions />
+						</div>
+					</div>
+				</div>
+				<div className="row">
+				    <Bookedslots />
+				</div>
+			</div>			
+		</Layout>
 	);
 }
