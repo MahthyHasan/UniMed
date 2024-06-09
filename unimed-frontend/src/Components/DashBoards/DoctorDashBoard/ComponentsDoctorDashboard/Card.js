5982988b45d0f1ef35d55e001d0d4bc83dadbb99
Mahthy Hasan <mahthyhasan01@gmail.com>
2024-06-10 04:51:04 +0530

    QrCode InterGrated FirstStep Completed

--- unimed-frontend/src/Components/DashBoards/DoctorDashBoard/ComponentsDoctorDashboard/Card.js
+++ unimed-frontend/src/Components/DashBoards/DoctorDashBoard/ComponentsDoctorDashboard/Card.js
@@ -2,46 +2,42 @@ import React from "react";
 import patient from "../../../../assets/icons/patient.svg";
 import totalpatient from "../../../../assets/icons/totalpatient.svg";
 import hours from "../../../../assets/icons/hours.svg";
-
-
+import TopInfoCards from "./TopInfoCards";
 
 export const Card = () => {
   return (
-    <div className="card--container">
-      <div className="card card-remaining">
-        <div className="card--title">
-          <h6>Remaining Patients in Counter</h6>
+    <>
+    <div class="col-12 col-md-4 sm">
+            <div class="p-3 mx-auto">
+              <TopInfoCards
+                cardCategoryStyle = 'cardRemaining'
+                cardTitle = 'Remaining Patients'
+                cardIconPath={patient}
+                cardData ='10'
+              />
+            </div>
         </div>
-        <div className="card--icon">
-          <img src={patient} alt="patient" className="input-label-icon" />
+        <div class="col-12 col-md-4 sm">
+            <div class="p-3 mx-auto">
+            <TopInfoCards
+                cardCategoryStyle = 'cardTotalPatient'
+                cardTitle = 'Total Patients'
+                cardIconPath={totalpatient}
+                cardData ='20'
+              />
+            </div>
         </div>
-        <div className="card--data">
-          <h3>10</h3>
-        </div>
-      </div>
-      <div className="card card-total">
-        <div className="card--title">
-          <h6>Total Channelled Patients</h6>
-        </div>
-        <div className="card--icon">
-          <img src={totalpatient} alt="patient" className="input-label-icon" />
-        </div>
-        <div className="card--data">
-          <h3>20</h3>
-        </div>
-      </div>
-      <div className="card card-hours">
-        <div className="card--title">
-          <h6>Total Channelled Hours for the Day</h6>
-        </div>
-        <div className="card--icon">
-          <img src={hours} alt="hours" className="input-label-icon" />
-        </div>
-        <div className="card--data">
-          <h3>03:40:23</h3>
-        </div>
-      </div>
-    </div>
+        <div class="col-12 col-md-4 sm">
+            <div class="p-3 mx-auto">
+            <TopInfoCards
+                cardCategoryStyle = 'cardChanelledHours'
+                cardTitle = 'Chanelled Time'
+                cardIconPath={hours}
+                cardData ='3:32:54 Hrs'
+              />
+            </div>
+        </div>    
+    </>
   );
 };
 
--- unimed-frontend/src/Components/DashBoards/DoctorDashBoard/ComponentsDoctorDashboard/QRCodeScanner.js
+++ unimed-frontend/src/Components/DashBoards/DoctorDashBoard/ComponentsDoctorDashboard/QRCodeScanner.js
@@ -1,49 +1,62 @@
-import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
+import React, { useState } from "react";
+import { QrReader } from "react-qr-reader";
+import "./qrCodeScanner.css";
 
 const QRCodeScanner = () => {
-  const [data, setData] = useState('No result');
-  const [scanning, setScanning] = useState(false);
+	const [data, setData] = useState("No result");
+	const [scanning, setScanning] = useState(true);
 
-  const handleScan = (result) => {
-    if (result?.text) {
-      setData(result.text);
-      setScanning(false);
-    }
-  };
+	const handleScan = (result) => {
+		if (result?.text) {
+			setData(result.text);
+			setScanning(false);
+		}
+	};
 
-  const handleError = (err) => {
-    console.error(err);
-  };
+	const handleError = (err) => {
+		console.error(err);
+	};
 
-  const startScanning = () => {
-    setData('No result');
-    setScanning(true);
-  };
+	const startScanning = () => {
+		setData("No result");
+		setScanning(true);
+	};
 
-  const stopScanning = () => {
-    setScanning(false);
-  };
+	const stopScanning = () => {
+		setScanning(false);
+	};
 
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px', width: '30%' }}>
-      <h1>QR Code Scanner</h1>
-      {scanning ? (
-        <>
-          <QrReader
-            onResult={handleScan}
-            onError={handleError}
-            constraints={{ facingMode: 'environment' }}
-            style={{ width: '200px', margin: '0 auto' }}
-          />
-          <button onClick={stopScanning} style={{ marginTop: '20px' }}>Stop Scanning</button>
-        </>
-      ) : (
-        <button onClick={startScanning} style={{ marginTop: '20px' }}>Start Scanning</button>
-      )}
-      <p>{data}</p>
-    </div>
-  );
+	return (
+		<div className="qrCodeScanner col">
+            <div>
+            <p className="qrCodeScannerIntroduction" >Please place The Qr Code inside the camera range.</p>
+            </div>			
+            <div className="qrCodeReader row">
+            {scanning ? (
+				<>
+                    
+					<QrReader
+                        className="ScanerPart"
+						onResult={handleScan}
+						onError={handleError}
+						constraints={{ facingMode: "environment" }}                        						
+					/>
+					<button onClick={stopScanning} className="btn btn-danger">
+						Stop Scanning
+					</button>
+				</>
+			) : (
+				<button onClick={startScanning} className="btn btn-success">
+					Start Scanning
+				</button>
+			)}
+            </div>
+            <div className="qrCodeResuts row">
+            <p>{data}</p>
+            </div>			
+			
+		</div>
+	);
 };
 
 export default QRCodeScanner;
