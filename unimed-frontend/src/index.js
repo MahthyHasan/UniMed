c6894c13dbb25ac1abf6b523b2b2f7c33eeb7c31
Tharushi_Vithanage <86551511+didulani123@users.noreply.github.com>
2024-06-18 12:50:42 +0530

    update

--- unimed-frontend/src/index.js
+++ unimed-frontend/src/index.js
@@ -7,6 +7,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import $ from "jquery";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
