e2cf5120cea96eecef6df0833a53b6ac8535e540
HasanMJMM <maheng907@gmail.com>
2024-06-22 14:31:31 +0530

    commit teset

--- unimed-frontend/src/Components/DashBoards/PatientDashBoard/ComponentsPatientDashboard/Table.js
+++ unimed-frontend/src/Components/DashBoards/PatientDashBoard/ComponentsPatientDashboard/Table.js
@@ -1,62 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "../../../../Css/Patient/Table.css";
-import details from "../../../../assets/icons2/details.svg"; 
-import Modal from './Modal';
 
-const demoData = [
-  {
-    recordId: 'CST20109MD003',
-    date: '04.02.2024',
-    symptoms: ['Symptoms 01', 'Symptoms 02', 'Symptoms 03', 'Symptoms 04'],
-    diagnoses: ['Diagnose 01', 'Diagnose 02', 'Diagnose 03', 'Diagnose 04'],
-    medicines: ['Medicine 01', 'Medicine 02', 'Medicine 03', 'Medicine 04'],
-  },
-  { 
-    recordId: 'CST20109MD004',
-    date: '19.02.2024',
-    symptoms: ['Symptoms 01', 'Symptoms 02', 'Symptoms 03', 'Symptoms 04'],
-    diagnoses: ['Diagnose 01', 'Diagnose 02', 'Diagnose 03', 'Diagnose 04'],
-    medicines: ['Medicine 01', 'Medicine 02', 'Medicine 03', 'Medicine 04'],
-  },
-  // ... other records
-];
-
-const Table = ({ data = demoData }) => {
-  const [showModal, setShowModal] = useState(false);
-  const [selectedRecord, setSelectedRecord] = useState(null);
-
-  const handleDetailsClick = (record) => {
-    setSelectedRecord(record);
-    setShowModal(true);
-  };
-
+const Table = ({ data }) => {
   return (
-    <>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Record ID</th>
-            <th>Date</th>
-            <th>Details</th>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Record ID</th>
+          <th>Date</th>
+          <th>Details</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            <td>{row.recordId}</td>
+            <td>{row.date}</td>
+            <td>
+              <button className="details-btn">Details</button>
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <tr key={index} >
-              <td>{row.recordId}</td>
-              <td>{row.date}</td>
-              <td>
-                <button className="details-btn" onClick={() => handleDetailsClick(row)}>
-                  <img src={details} alt="details icon" className="details-icon" />
-                  Details
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <Modal show={showModal} onClose={() => setShowModal(false)} record={selectedRecord} />
-    </>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
