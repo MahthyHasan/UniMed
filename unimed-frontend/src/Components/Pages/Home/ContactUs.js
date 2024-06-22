460ae695ce66800c0c166b49aae9219db44bcb8c
Tharushi_Vithanage <86551511+didulani123@users.noreply.github.com>
2024-06-22 20:11:06 +0530

    required

--- unimed-frontend/src/Components/Pages/Home/ContactUs.js
+++ unimed-frontend/src/Components/Pages/Home/ContactUs.js
@@ -47,94 +47,97 @@ const ContactUs = () => {
 
   return (
     <>
-    <NavBar />
-    <section className="contact-form">
-      <h1 className="heading">Get In Touch!</h1>
-      <p className="para">
-        At our university's medical center, we are committed to providing
-        exceptional healthcare services. Our dedicated team of professionals is
-        here to support you with comprehensive medical care and innovative
-        treatment options.
-      </p>
+      <NavBar />
+      <section className="contact-form">
+        <h1 className="heading">Get In Touch!</h1>
+        <p className="para">
+          At our university's medical center, we are committed to providing
+          exceptional healthcare services. Our dedicated team of professionals
+          is here to support you with comprehensive medical care and innovative
+          treatment options.
+        </p>
 
-      <div className="contactForm">
-        <form onSubmit={onSubmit}>
-          <h1 className="sub-heading">Need Support!</h1>
-          <input
-            type="text"
-            className="input"
-            id="name"
-            placeholder="your name"
-            value={formData.name}
-            onChange={onInputChange}
-          />
-          <input
-            type="email"
-            className="input"
-            id="email"
-            placeholder="your email"
-            value={formData.email}
-            onChange={onInputChange}
-          />
+        <div className="contactForm">
+          <form onSubmit={onSubmit}>
+            <h1 className="sub-heading">Need Support!</h1>
+            <input
+              type="text"
+              className="input"
+              id="name"
+              placeholder="your name"
+              value={formData.name}
+              onChange={onInputChange}
+              required
+            />
+            <input
+              type="email"
+              className="input"
+              id="email"
+              placeholder="your email"
+              value={formData.email}
+              onChange={onInputChange}
+              required
+            />
 
-          <textarea
-            className="input"
-            cols="30"
-            rows="5"
-            id="message"
-            placeholder="Your message..."
-            value={formData.message}
-            onChange={onInputChange}
-          ></textarea>
-          <button type="submit" className="input submit">
-            Send Message
-          </button>
-        </form>
+            <textarea
+              className="input"
+              cols="30"
+              rows="5"
+              id="message"
+              placeholder="Your message..."
+              value={formData.message}
+              onChange={onInputChange}
+              required
+            ></textarea>
+            <button type="submit" className="input submit">
+              Send Message
+            </button>
+          </form>
 
-        <div className="map-container">
-          <div className="mapBg"></div>
-          <div className="map">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63313.21215607468!2d81.05359324133163!3d6.988228947404726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae62e3ed1dd0eb9%3A0x7261eb4e22c7a101!2sUva%20Wellassa%20University%20Medical%20Center!5e0!3m2!1sen!2slk!4v1683112929861!5m2!1sen!2slk"
-              width="600"
-              height="450"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
+          <div className="map-container">
+            <div className="mapBg"></div>
+            <div className="map">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63313.21215607468!2d81.05359324133163!3d6.988228947404726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae62e3ed1dd0eb9%3A0x7261eb4e22c7a101!2sUva%20Wellassa%20University%20Medical%20Center!5e0!3m2!1sen!2slk!4v1683112929861!5m2!1sen!2slk"
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="contactMethod">
-        <div className="method">
-          <BsGeoAlt className="contactIcon" />
-          <article className="text">
-            <h1 className="sub-heading">Location</h1>
-            <p className="para">
-              The Medical Center of Uva Wellassa University of Sri Lanka
-            </p>
-          </article>
-        </div>
+        <div className="contactMethod">
+          <div className="method">
+            <BsGeoAlt className="contactIcon" />
+            <article className="text">
+              <h1 className="sub-heading">Location</h1>
+              <p className="para">
+                The Medical Center of Uva Wellassa University of Sri Lanka
+              </p>
+            </article>
+          </div>
 
-        <div className="method email-method">
-          <BsEnvelope className="contactIcon" />
-          <article className="text">
-            <h1 className="sub-heading">Email</h1>
-            <p className="para">umo@uwu.ac.lk</p>
-          </article>
-        </div>
+          <div className="method email-method">
+            <BsEnvelope className="contactIcon" />
+            <article className="text">
+              <h1 className="sub-heading">Email</h1>
+              <p className="para">umo@uwu.ac.lk</p>
+            </article>
+          </div>
 
-        <div className="method">
-          <BsPhone className="contactIcon" />
-          <article className="text">
-            <h1 className="sub-heading">Phone</h1>
-            <p className="para">+94 55 2226477</p>
-          </article>
+          <div className="method">
+            <BsPhone className="contactIcon" />
+            <article className="text">
+              <h1 className="sub-heading">Phone</h1>
+              <p className="para">+94 55 2226477</p>
+            </article>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
