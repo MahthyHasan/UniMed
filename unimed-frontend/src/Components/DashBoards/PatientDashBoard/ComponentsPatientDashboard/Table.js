6122639fa55510a8c7093ac9ceb508c2d6578ab0
Chamara D Rodrigo <108047725+ChamaraRodrigo@users.noreply.github.com>
2024-06-23 02:35:21 +0530

    patient ui

--- unimed-frontend/src/Components/DashBoards/PatientDashBoard/ComponentsPatientDashboard/Table.js
+++ unimed-frontend/src/Components/DashBoards/PatientDashBoard/ComponentsPatientDashboard/Table.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import "../../../../Css/Patient/Table.css";
-import details from "../../../../assets/icons2/details.svg"; 
+import '../../../../Css/Patient/Table.css';
+import details from '../../../../assets/icons2/details.svg';
 import Modal from './Modal';
 
 const demoData = [
@@ -11,7 +11,7 @@ const demoData = [
     diagnoses: ['Diagnose 01', 'Diagnose 02', 'Diagnose 03', 'Diagnose 04'],
     medicines: ['Medicine 01', 'Medicine 02', 'Medicine 03', 'Medicine 04'],
   },
-  { 
+  {
     recordId: 'CST20109MD004',
     date: '19.02.2024',
     symptoms: ['Symptoms 01', 'Symptoms 02', 'Symptoms 03', 'Symptoms 04'],
@@ -42,7 +42,7 @@ const Table = ({ data = demoData }) => {
         </thead>
         <tbody>
           {data.map((row, index) => (
-            <tr key={index} >
+            <tr key={index}>
               <td>{row.recordId}</td>
               <td>{row.date}</td>
               <td>
