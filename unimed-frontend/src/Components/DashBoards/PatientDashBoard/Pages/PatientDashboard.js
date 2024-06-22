b54547949a1262419c762f45334a3349ad2b2d3a
Chamara D Rodrigo <108047725+ChamaraRodrigo@users.noreply.github.com>
2024-06-23 02:21:02 +0530

    patient ui

--- unimed-frontend/src/Components/DashBoards/PatientDashBoard/Pages/PatientDashboard.js
+++ unimed-frontend/src/Components/DashBoards/PatientDashBoard/Pages/PatientDashboard.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Personal_Info from "../ComponentsPatientDashboard/Personal_Info"; 
 import Layout from "../../layout/PatientLayout/PatientLayout";
-import Table from '../ComponentsPatientDashboard/Table';
+
+
 
 export default function PatientDashboard() {
   const [username, setUsername] = useState("");
@@ -14,23 +15,12 @@ export default function PatientDashboard() {
     }
   }, []);
 
-  const studentInfo = {
-    name: "M.J.M.M Hasan",
-    id: "UWU/CST/20/109",
-    age: "25",
-    gender: "Male",
-    height: "160cm",
-    weight: "65kg",
-    bloodgrp: "O+",
-    bmi: "12"
-  };
-
   return (
     <div>
       <Layout>
-        <h1>{username}</h1> {/* Replace 'Patient Name' with the retrieved username */}
-        <Personal_Info {...studentInfo} />
-        <Table />
+        <h1>{username}</h1>{" "}
+        {/* Replace 'Patient' Name' with the retrieved username */}
+        <Personal_Info />
       </Layout>
     </div>
   );
