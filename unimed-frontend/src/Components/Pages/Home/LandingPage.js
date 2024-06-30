43c80e59f8aa12c877721c90b7f0420bfa5265f6
YashodhaKulathunga99 <yashodhakulathunga@gmail.com>
2024-06-30 20:52:51 +0700

    Revert "Merge branch 'main' of https://github.com/yashodhakulathunga99/UniMedNewRepo"
    This reverts commit 3512aa2fbec593e70a0bd98308d69ca29461ca5f, reversing
    changes made to 78e11d04c06e0deaec64678664e0fe2b9c1bb2e8.

--- unimed-frontend/src/Components/Pages/Home/LandingPage.js
+++ unimed-frontend/src/Components/Pages/Home/LandingPage.js
@@ -2,20 +2,12 @@ import React from 'react'
 import NavBar from './navbar'
 import Header from './Header'
 import "./styleHP.css"
-import Footer from './footer'
 
 function LandingPage() {
   return (
     <div className='container'>
         <NavBar />
-        <Header />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <Footer className='footerOnLandingPage1' />      
+        <Header />      
     </div>
   )
 }
