0b5bf8bc5b217dedd6d383cc4d1073c021241a54
Chamara D Rodrigo <108047725+ChamaraRodrigo@users.noreply.github.com>
2024-07-02 01:16:42 +0530

    UI update

--- unimed-frontend/src/Components/DashBoards/PatientDashBoard/ComponentsPatientDashboard/Button.js
+++ unimed-frontend/src/Components/DashBoards/PatientDashBoard/ComponentsPatientDashboard/Button.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import "../../../../Css/Patient/Button.css";
-
+import Form from "./Form";
 
 const Button = () => {
+
+  const handleButtonClick = () => {
+    const baseUrl = window.location.origin; // Get the base URL of the current page
+    window.open(baseUrl + '/ComponentsPatientDashboard/Form.js', '_blank');
+  };
+  
+
   return (
-    <button className="button">
-      Apply Now
-      <svg fill="currentColor" viewBox="0 0 24 24" className="icon">
-        <path clipRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fillRule="evenodd"></path>
-      </svg>
+    <div>
+      <button className="custom-button" onClick={handleButtonClick}>
+      General Checkup
+      </button>
+    <br></br>
+    <hr></hr>
+    
+    <button className="custom-button" onClick={handleButtonClick}>
+      Request Medical Report
     </button>
+  </div>
   );
 };
 
--- unimed-frontend/src/Components/DashBoards/PatientDashBoard/Pages/PatientDashboard.js
+++ unimed-frontend/src/Components/DashBoards/PatientDashBoard/Pages/PatientDashboard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Personal_Info from "../ComponentsPatientDashboard/Personal_Info"; 
 import Layout from "../../layout/PatientLayout/PatientLayout";
 import Card from '../ComponentsPatientDashboard/Card';
-
+import Button from "../ComponentsPatientDashboard/Button";
 
 export default function PatientDashboard() {
   const [username, setUsername] = useState("");
