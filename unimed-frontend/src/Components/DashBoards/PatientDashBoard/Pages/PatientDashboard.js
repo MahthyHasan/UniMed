1678639e3cab6486c3b0a016e47116323a1fc360
Chamara D Rodrigo <108047725+ChamaraRodrigo@users.noreply.github.com>
2024-07-06 20:10:08 +0530

    patient

--- unimed-frontend/src/Components/DashBoards/PatientDashBoard/Pages/PatientDashboard.js
+++ unimed-frontend/src/Components/DashBoards/PatientDashBoard/Pages/PatientDashboard.js
@@ -1,70 +1,9 @@
-// import React, { useEffect, useState } from "react";
-// import Personal_Info from "../ComponentsPatientDashboard/Personal_Info"; 
-// import Layout from "../../layout/PatientLayout/PatientLayout";
-// import Card from '../ComponentsPatientDashboard/Card';
-// import Button from "../ComponentsPatientDashboard/Button";
-// import DocAvailability from "../ComponentsPatientDashboard/DocAvailability";
-// import AddAppointment from "../ComponentsPatientDashboard/AddAppointment"; // Import AddAppointment
-// import ViewAppointment from "../ComponentsPatientDashboard/ViewAppointment"; // Import ViewAppointment
-// import AppointmentList from "../ComponentsPatientDashboard/AppointmentList"; // Import AppointmentList
-
-// export default function PatientDashboard() {
-//   const [username, setUsername] = useState("");
-//   const [selectedAppointment, setSelectedAppointment] = useState(null);
-
-//   useEffect(() => {
-//     // Retrieve the username from localStorage
-//     const storedUsername = localStorage.getItem("username");
-//     if (storedUsername) {
-//       setUsername(storedUsername);
-//     }
-//   }, []);
-
-//   const studentInfo = {
-//     name: "M.J.M.M Hasan",
-//     id: "UWU/CST/20/109",
-//     age: "25",
-//     gender: "Male",
-//     height: "160cm",
-//     weight: "65kg",
-//     bloodgrp: "O+",
-//     bmi: "12"
-//   };
-
-//   const handleAddAppointment = (startTime, endTime, name, phone) => {
-//     // Add logic to handle adding an appointment
-//     // This can dispatch an action or update state
-//   };
-
-//   const handleViewAppointment = (appointment) => {
-//     setSelectedAppointment(appointment);
-//   };
-
-//   return (
-//     <div>
-//       <Layout>
-//         <h1>{username}</h1> 
-//         {/* <Personal_Info {...studentInfo} /> */}
-//         <br /><br /><br />
-//         {/* <AppointmentList onViewAppointment={handleViewAppointment} /> */}
-//         <br /><br /><br />
-//         {/* <Button /> */}
-//         <br /><br /><br />
-//         {/* <DocAvailability doctorName="Dr. John Doe" isAvailable={true} />  */}
-//          <AddAppointment onAddAppointment={handleAddAppointment} /> 
-//         <ViewAppointment appointment={selectedAppointment} />  
-//       </Layout> 
-//     </div>
-//   );
-// }
-
 import React, { useEffect, useState } from "react";
 import Personal_Info from "../ComponentsPatientDashboard/Personal_Info"; 
 import Layout from "../../layout/PatientLayout/PatientLayout";
 import DocAvailability from "../ComponentsPatientDashboard/DocAvailability";
 import DayScheduleButton from "../ComponentsPatientDashboard/DayScheduleButton";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import GenerateQRCode from "../../DoctorDashBoard/ComponentsDoctorDashboard/GenerateQRCode";
 export default function PatientDashboard() {
   const [username, setUsername] = useState("");
   const [selectedAppointment, setSelectedAppointment] = useState(null);
@@ -98,24 +37,20 @@ export default function PatientDashboard() {
   return (
     <div>
       <Layout>
-
-        <h1>{username}</h1> {/* Replace 'Patient Name' with the retrieved username */}
-        {/* <PersonalInfo {...studentInfo} /> */}
-        <br /><br /><br />
-        {/* Appointment Details Card */}
-        <Card type="appointment" />
-        <br /><br /><br />
-        {/* Button Component */}
-        {/* <Button /> */}
-        <br /><br /><br />
-        {/* Doctor Availability Component */}
-        <DocAvailability doctorName="Dr. John Doe" isAvailable={true} /> {/* Pass appropriate props */}
-        <Personal_Info {...studentInfo} />
-        <br />
-        <Card />
-        <br />
+        <h1>{username}</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <Personal_Info {...studentInfo} />
+            </div>
+            <div className="col-md-6 d-flex flex-column align-items-center">
+              <DocAvailability doctorName="Dr. John Doe" isAvailable={true} />
+              <DayScheduleButton />
+              {/* <GenerateQRCode /> */}
+            </div>
+          </div>
+        </div>
       </Layout>
-
     </div>
   );
 }
