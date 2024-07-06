5c221a4f48e4d7ca35e0c81b5ce40a9ec21a1ba6
Mahthy Hasan <mahthyhasan01@gmail.com>
2024-07-06 21:53:25 +0530

    Merge branch 'main' of https://github.com/MahthyHasan/UniMed

--- unimed-frontend/src/Components/DashBoards/DoctorDashBoard/ComponentsDoctorDashboard/Bookedslots.js
+++ unimed-frontend/src/Components/DashBoards/DoctorDashBoard/ComponentsDoctorDashboard/Bookedslots.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Bookedslots = () => {
   return (
     <div >
-      <div className="justify-content-center">
+      <div className="booked-slots-text justify-content-center">
       <div className="text">Booked Time Slots</div>
       </div>
       <div className="justify-content-center gap-4">
