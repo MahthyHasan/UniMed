f3aca104c5252a1e71f609025ff04cec4806f6f2
Mahthy Hasan <mahthyhasan01@gmail.com>
2024-07-07 19:36:27 +0530

    update phamcay dashboard link

--- unimed-frontend/src/Components/Login/Pharmacist/PharmacistLogin.js
+++ unimed-frontend/src/Components/Login/Pharmacist/PharmacistLogin.js
@@ -61,7 +61,7 @@ function PharmacistLogin() {
           // Automatically dismiss the alert after 3 seconds and navigate to home page
           setTimeout(() => {
             setAlertVisible(false);
-            navigate('/home');
+            navigate('/ClinicForPharmacist');
           }, 3000);
         } catch (error) {
           console.error('Login failed:', error);
