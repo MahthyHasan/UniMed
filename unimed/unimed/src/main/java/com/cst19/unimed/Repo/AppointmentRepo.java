38f79fec83204691b077e66581f845b90b51faca
Chamara D Rodrigo <108047725+ChamaraRodrigo@users.noreply.github.com>
2024-10-07 14:58:51 +0530

    Appointment Backend

--- unimed/unimed/src/main/java/com/cst19/unimed/Repo/AppointmentRepo.java
+++ unimed/unimed/src/main/java/com/cst19/unimed/Repo/AppointmentRepo.java
@@ -4,5 +4,4 @@ import com.cst19.unimed.Entity.Appointment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AppointmentRepo extends MongoRepository<Appointment, String> {
-
 }
